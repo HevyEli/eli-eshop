443f24ca422a1eaa78c3fdd3ef74a428a3be3d61
Michal Elias <michal1.elias@t-mobile.cz>
2023-08-16 11:01:29 +0200

    changed method to findByUsername

--- src/main/java/org/eli/user/repository/UserRepository.java
+++ src/main/java/org/eli/user/repository/UserRepository.java
@@ -3,6 +3,6 @@ import org.eli.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends  JpaRepository <User, Long> {
-    User findUserByUserName (String username);
+    User findByUsername (String username);
 
 }
